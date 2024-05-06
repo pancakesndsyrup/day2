f35ba15cb317f0312343b30e190fefabf13e386e
Kaur <akaur3@cps.k12.il.us>
2024-05-06 12:13:59 -0500

    first commit

---
+++ classworkAssignment.py
@@ -0,0 +1,24 @@
+Grade = int (input("What's your grade level?"))
+
+if Grade >= 9:
+    print("Eligible to check out book on own")
+
+elif Grade < 8 + permission
+    print("eligible")
+else:
+    print("Not eligible")
+
+permission = input("Do you have permission from your parents? Yes or no?")
+if permission == True:
+    print("What's the name of the book?")
+else:
+    print("Not eligible")
+
+returnn = input("Do you want to return a book?")
+
+if returnn == yes:
+    print("What's the name of the book?")
+
+weather = input("Is it raining?")
+if weather == yes:
+    print("Pick a book and read at the library")
---
+++ loops.py
@@ -0,0 +1,32 @@
+#nums = [1, 2, 3, 4 ,5]
+
+#nested for loop
+#for num in nums:
+    #for letter in 'abc':
+        #print(num, letter)
+   # if num == 3:
+      #  print('Found!')
+      #  continue  
+   # print(num)
+
+
+
+#ranges
+#for i in range(1001):
+#    print(i)
+
+x = 0
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
+
+#while True:
+ #   print("Enter your name")
+
+#  name = input()
+ #    print("your name is " +name)
+#    break
+#else:
+#   print("Thank you")
